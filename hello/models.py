23bb202e19fb6cc17447a8786fec61a1a0f8c26b
William M. <willloganmiller@gmail.com>
2026-08-20 12:12:23 -0400

    Changed entry_text to use TextField

--- hello/models.py
+++ hello/models.py
@@ -4,11 +4,9 @@ from django.conf import settings
 
 # Create your models here.
 class Entry(models.Model):
-    entry_title = models.CharField(default="Untitled")
-    entry_text = models.CharField()
+    entry_title = models.CharField(default="Untitled", max_length=64)
+    entry_text = models.TextField()
     entry_text_wordcount = models.PositiveIntegerField(default=1)
-    tags = models.CharField(null=True, blank=True)  # A comma-separated list of tags
-                                                    # TODO: Actually implement this
     creation_datetime = models.DateTimeField()
     last_edit_datetime = models.DateTimeField(null=True, blank=True)
     is_secret = models.BooleanField(default=False)
